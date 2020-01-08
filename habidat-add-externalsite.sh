6c9f0e015f0476d6b15d5868fcc0cbbb6a4b96d0
Florian Humer <florian.humer@gmail.com>
2020-01-08 14:34:32 +0100

    bugfix bootstrap

--- habidat-add-externalsite.sh
+++ habidat-add-externalsite.sh
@@ -17,16 +17,16 @@ then
 elif [ "$1" == "mediawiki" ] && [ ! -z $HABIDAT_MEDIAWIKI_SUBDOMAIN ] 
 then
 	NEW_EXTERNAL_SITE+=$(printf "$EXTERNAL_SITES_TEMPLATE" "$INDEX" "wiki.png" "$INDEX" "Mediawiki" "$HABIDAT_PROTOCOL://$HABIDAT_MEDIAWIKI_SUBDOMAIN.$HABIDAT_DOMAIN")
-elif [ "$1" == "dokuwiki" ] && [ ! -z "$HABIDAT_DOKUWIKI_SUBDOMAIN" ] 
+elif [ "$1" == "dokuwiki" ] && [ ! -z $HABIDAT_DOKUWIKI_SUBDOMAIN ] 
 then
 	NEW_EXTERNAL_SITE+=$(printf "$EXTERNAL_SITES_TEMPLATE" "$INDEX" "wiki.png" "$INDEX" "Dokuwiki" "$HABIDAT_PROTOCOL://$HABIDAT_DOKUWIKI_SUBDOMAIN.$HABIDAT_DOMAIN")
-elif [ "$1" == "user" ] && [ ! -z "$HABIDAT_DOKUWIKI_SUBDOMAIN" ] 
+elif [ "$1" == "user" ] && [ ! -z $HABIDAT_USER_SUBDOMAIN ] 
 then
 	NEW_EXTERNAL_SITE+=$(printf "$EXTERNAL_SITES_TEMPLATE" "$INDEX" "user.png" "$INDEX" "User*innen" "$HABIDAT_PROTOCOL://$HABIDAT_USER_SUBDOMAIN.$HABIDAT_DOMAIN")
-elif [ "$1" == "direktkredit" ] && [ ! -z "$HABIDAT_DOKUWIKI_SUBDOMAIN" ] 
+elif [ "$1" == "direktkredit" ] && [ ! -z $HABIDAT_DIREKTKREDIT_SUBDOMAIN ] 
 then
 	NEW_EXTERNAL_SITE+=$(printf "$EXTERNAL_SITES_TEMPLATE" "$INDEX" "direktkredite.png" "$INDEX" "Direktkredite" "$HABIDAT_PROTOCOL://$HABIDAT_DIREKTKREDIT_SUBDOMAIN.$HABIDAT_DOMAIN")
-elif [ "$1" == "mailtrain" ] && [ ! -z "$HABIDAT_DOKUWIKI_SUBDOMAIN" ] 
+elif [ "$1" == "mailtrain" ] && [ ! -z $HABIDAT_MAILTRAIN_SUBDOMAIN ] 
 then
 	NEW_EXTERNAL_SITE+=$(printf "$EXTERNAL_SITES_TEMPLATE" "$INDEX" "newsletter.png" "$INDEX" "Newsletter" "$HABIDAT_PROTOCOL://$HABIDAT_MAILTRAIN_SUBDOMAIN.$HABIDAT_DOMAIN")
 fi
