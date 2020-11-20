4996dc457958f3bb353effc8f349c578066fb87c
Florian Humer <hf@muehlboeck.com>
2020-11-20 19:54:51 +0100

    update to 20

--- habidat-afterupdate.sh
+++ habidat-afterupdate.sh
@@ -90,7 +90,7 @@ php occ maintenance:mode -n --off
 
 #cp /fixes/LoginController.php /var/www/html/core/Controller/LoginController.php
 
-if [ $HABIDAT_SSO == "true" ]
-then
-	cp /fixes/SAMLController.php /var/www/html/custom_apps/user_saml/lib/Controller/SAMLController.php
-fi	
+#if [ $HABIDAT_SSO == "true" ]
+#then
+#	cp /fixes/SAMLController.php /var/www/html/custom_apps/user_saml/lib/Controller/SAMLController.php
+#fi	
--- habidat-fixes.sh
+++ habidat-fixes.sh
@@ -3,7 +3,7 @@ set +x
 
 #cp /fixes/LoginController.php /var/www/html/core/Controller/LoginController.php
 
-if [ $HABIDAT_SSO == "true" ]
-then
-	cp /fixes/SAMLController.php /var/www/html/custom_apps/user_saml/lib/Controller/SAMLController.php
-fi	
+#if [ $HABIDAT_SSO == "true" ]
+#then
+#	cp /fixes/SAMLController.php /var/www/html/custom_apps/user_saml/lib/Controller/SAMLController.php
+#fi	
